99a4049f588a874a93083ab4b1ea08bf422209dd
jchan34 <54876929+jchan34@users.noreply.github.com>
2020-06-11 14:18:20 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+
 #include <Arduino.h>
 #include <Servo.h>
 #include <Wire.h>
@@ -11,19 +12,30 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 #define MOTOR_A PA_7
 #define MOTOR_B PA_6
 #define ANALOG_IN PB1
-
+#define PWMFREQ 2000
 #define SERVOPIN PA1
-
+void handle_interrupt();
+void handle_interrupt_schmitt();
+volatile int i = 0;
+volatile int j = 0;
+int reading;
+int angle;
 Servo servo;
-
+int servoAngle = 0;
+boolean hitServoEdge = false;
 void setup()
 {
-
+  //  pinMode(INTERRUPTPIN, INPUT_PULLUP);
+  // pinMode(TAPESENSOR, INPUT_ANALOG);
+  //pinMode(MOTOR_A, OUTPUT);
+  //pinMode(MOTOR_B, OUTPUT);
   pinMode(ANALOG_IN, INPUT_ANALOG);
   servo.attach(SERVOPIN);
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+
   // Displays Adafruit logo by default. call clearDisplay immediately if you don't want this.
   display.display();
+  delay(2000);
 
   // Displays "Hello world!" on the screen
   display.clearDisplay();
@@ -32,20 +44,37 @@ void setup()
   display.setCursor(0, 0);
 
   display.clearDisplay();
+  pwm_start(MOTOR_A, PWMFREQ, 0, RESOLUTION_10B_COMPARE_FORMAT);
+  pwm_start(MOTOR_B, PWMFREQ, 0, RESOLUTION_10B_COMPARE_FORMAT);
+}
+
+void handle_interrupt()
+{
+  i++;
 }
 
 void loop()
 {
 
   display.clearDisplay();
-  display.setCursor(0, 0);
 
-  servo.write(0);
-  delay(1000);
-  servo.write(45);
-  delay(1000);
-  servo.write(90);
-  delay(1000);
-  servo.write(45);
-  delay(1000);
+  if (analogRead(ANALOG_IN) > 512)
+  {
+    pwm_start(MOTOR_A, PWMFREQ, 2 * (analogRead(ANALOG_IN) - 512), RESOLUTION_10B_COMPARE_FORMAT);
+    pwm_start(MOTOR_B, PWMFREQ, 0, RESOLUTION_10B_COMPARE_FORMAT);
+    display.println("Forwards");
+    display.setCursor(0, 15);
+    display.println(2 * (analogRead(ANALOG_IN) - 512));
+    display.display();
+  }
+  else
+  {
+    pwm_start(MOTOR_A, PWMFREQ, 0, RESOLUTION_10B_COMPARE_FORMAT);
+    pwm_start(MOTOR_B, PWMFREQ, -2 * (analogRead(ANALOG_IN) - 511), RESOLUTION_10B_COMPARE_FORMAT);
+    display.println("Backwards");
+    display.setCursor(0, 15);
+    display.println(-2 * (analogRead(ANALOG_IN) - 511));
+    display.display();
+  }
+
 };
